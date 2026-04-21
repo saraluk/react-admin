736112d3f0510067d26b80f334a44affa6868740
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-21 14:02:10 +0700

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <header
+        className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow"
+        data-bs-theme="dark"
+      >
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white"
+          href="#"
         >
-          Learn React
+          Company name
         </a>
+        <button
+          className="navbar-toggler position-absolute d-md-none collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <input
+          className="form-control form-control-dark w-100"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#">
+              Sign out
+            </a>
+          </li>
+        </ul>
       </header>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+            <div
+              className="offcanvas-md offcanvas-end bg-body-tertiary"
+              tabIndex={-1}
+              id="sidebarMenu"
+              aria-labelledby="sidebarMenuLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="sidebarMenuLabel">
+                  Company name
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  data-bs-target="#sidebarMenu"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link d-flex align-items-center gap-2 active"
+                      aria-current="page"
+                      href="#"
+                    >
+                      <svg className="bi" aria-hidden="true">
+                        <use xlinkHref="#house-fill"></use>
+                      </svg>
+                      Dashboard
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link d-flex align-items-center gap-2"
+                      href="#"
+                    >
+                      <svg className="bi" aria-hidden="true">
+                        <use xlinkHref="#file-earmark"></use>
+                      </svg>
+                      Orders
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link d-flex align-items-center gap-2"
+                      href="#"
+                    >
+                      <svg className="bi" aria-hidden="true">
+                        <use xlinkHref="#cart"></use>
+                      </svg>
+                      Products
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link d-flex align-items-center gap-2"
+                      href="#"
+                    >
+                      <svg className="bi" aria-hidden="true">
+                        <use xlinkHref="#people"></use>
+                      </svg>
+                      Customers
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link d-flex align-items-center gap-2"
+                      href="#"
+                    >
+                      <svg className="bi" aria-hidden="true">
+                        <use xlinkHref="#graph-up"></use>
+                      </svg>
+                      Reports
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="table-responsive small">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th> <th scope="col">Header</th>
+              <th scope="col">Header</th> <th scope="col">Header</th>
+              <th scope="col">Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td> <td>random</td> <td>data</td> <td>placeholder</td>
+              <td>text</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
