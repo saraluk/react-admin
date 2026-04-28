c067e78f5657657b7fd27dbc064053c194326bbd
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-04-28 09:58:09 +0700

    users page pagination

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,19 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Wrapper } from "../../components/Wrapper";
 import axios from "axios";
 import { User } from "../../models/user";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
 
   useEffect(() => {
     const getUsers = async () => {
-      const { data } = await axios.get("users");
+      const { data } = await axios.get(`users?page=${page}`);
       setUsers(data.data);
+      setLastPage(data.meta.last_page);
     };
 
     getUsers();
-  }, []);
+  }, [page]);
+
+  const handleNextClick = () => {
+    if (page < lastPage) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const handlePreviousClick = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNumberClick = (page: number) => {
+    setPage(page);
+  };
+
+  const paginatedNumbers = useMemo(() => {
+    const length = lastPage > 3 ? 3 : lastPage;
+    const pages = Array.from({ length }, (_, i) => i + 1);
+    return pages;
+  }, [lastPage]);
+
   return (
     <Wrapper>
       <div className="table-responsive small">
@@ -42,6 +68,36 @@ export default function Users() {
           </tbody>
         </table>
       </div>
+
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${page === 1 ? "disabled" : ""}`}>
+            <a href="#" className="page-link" onClick={handlePreviousClick}>
+              Previous
+            </a>
+          </li>
+          {paginatedNumbers.map((num) => (
+            <li className={`page-item ${page === num ? "active" : ""}`}>
+              <a
+                className="page-link"
+                href="#"
+                onClick={() => handleNumberClick(num)}
+              >
+                {num}
+              </a>
+            </li>
+          ))}
+          <li className="page-item">
+            <a
+              className={`page-link ${page === lastPage ? "disabled" : ""}`}
+              href="#"
+              onClick={handleNextClick}
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </Wrapper>
   );
 }
